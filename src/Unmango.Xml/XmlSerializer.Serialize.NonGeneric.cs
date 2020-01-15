ae4009492dd02dc149806cc7ee2bcb86bb70797b
Erik Rasmussen <ecr.referee@hotmail.com>
2020-01-14 21:02:39 -0600

    Somehow more overloads

--- src/Unmango.Xml/XmlSerializer.Serialize.NonGeneric.cs
+++ src/Unmango.Xml/XmlSerializer.Serialize.NonGeneric.cs
@@ -6,42 +6,66 @@ namespace Unmango.Xml
 {
     public static partial class XmlSerializer
     {
-        public static byte[] Serialize(object value)
+        public static byte[] Serialize(object value, XmlSerializerOptions? options = null)
         {
             throw new NotImplementedException();
         }
 
-        public static byte[] Serialize(Type type, object value)
+        public static byte[] Serialize(
+            Type type,
+            object value,
+            XmlSerializerOptions? options = null)
         {
             throw new NotImplementedException();
         }
 
-        public static void Serialize(ref XmlWriter writer, object value)
+        public static void Serialize(
+            ref XmlWriter writer,
+            object value,
+            XmlSerializerOptions? options = null)
         {
             throw new NotImplementedException();
         }
 
-        public static void Serialize(Type type, ref XmlWriter writer, object value)
+        public static void Serialize(
+            Type type,
+            ref XmlWriter writer,
+            object value,
+            XmlSerializerOptions? options = null)
         {
             throw new NotImplementedException();
         }
 
-        public static void Serialize(PipeWriter writer, object value)
+        public static void Serialize(
+            PipeWriter writer,
+            object value,
+            XmlSerializerOptions? options = null)
         {
             throw new NotImplementedException();
         }
 
-        public static void Serialize(Type type, PipeWriter writer, object value)
+        public static void Serialize(
+            Type type,
+            PipeWriter writer,
+            object value,
+            XmlSerializerOptions? options = null)
         {
             throw new NotImplementedException();
         }
 
-        public static void Serialize(Stream stream, object value)
+        public static void Serialize(
+            Stream stream,
+            object value,
+            XmlSerializerOptions? options = null)
         {
             throw new NotImplementedException();
         }
 
-        public static void Serialize(Type type, Stream stream, object value)
+        public static void Serialize(
+            Type type,
+            Stream stream,
+            object value,
+            XmlSerializerOptions? options = null)
         {
             throw new NotImplementedException();
         }
