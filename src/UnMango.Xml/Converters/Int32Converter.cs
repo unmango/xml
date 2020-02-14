ebc5f1e0455944d1ee4ea4e0298afa8c78aec080
Erik Rasmussen <ecr.referee@hotmail.com>
2020-02-13 22:24:56 -0600

    Add a converter so the converts folder sticks around

---
+++ src/UnMango.Xml/Converters/Int32Converter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnMango.Xml.Converters
+{
+    public class Int32Converter : XmlConverter<int>
+    {
+        public override int Read(ref XmlReader reader, Type typeToConvert, XmlSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(ref XmlWriter writer, int value, XmlSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
