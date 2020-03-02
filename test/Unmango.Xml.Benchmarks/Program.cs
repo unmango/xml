ec2d36846760060fba75bbda929d962ac7a4d159
Erik Rasmussen <ecr.referee@hotmail.com>
2020-03-02 15:15:23 -0600

    Add benchmark project

---
+++ test/Unmango.Xml.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unmango.Xml.Benchmarks
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
