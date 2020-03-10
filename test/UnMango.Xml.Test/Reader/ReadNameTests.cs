6dad87922c7991462d435048fa5c5c0457ec83ed
Erik Rasmussen <ecr.referee@hotmail.com>
2020-03-10 13:40:40 -0500

    Move character arrays to static test data class

--- test/UnMango.Xml.Test/Reader/ReadNameTests.cs
+++ test/UnMango.Xml.Test/Reader/ReadNameTests.cs
@@ -8,45 +8,25 @@ namespace UnMango.Xml.Test.Reader
     [Trait("Category", "Unit")]
     public class ReadNameTests
     {
-        private static readonly char[] _validNameCharacters = new[]
-        {
-            '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-        };
-
-        private static readonly char[] _invalidNameCharacters = new[]
-        {
-            ';', '^', '`', '@', '['
-        };
-
-        private static readonly char[] _validStartNameCharacters = new[]
-        {
-            ':', 'A', 'Z', '_', 'a', 'z'
-        };
-
-        private static readonly char[] _invalidStartNameCharacters = new[]
-        {
-            '9', ';', '^', '`', '@', '['
-        };
-
         private static readonly IEnumerable<char> _validNameInvalidStartNameIntersection =
-            _validNameCharacters.Intersect(_invalidStartNameCharacters);
+            TestData.ValidNameCharacters.Intersect(TestData.InvalidStartNameCharacters);
 
         public static readonly IEnumerable<object[]> Invalid_Start_Name_Character_Names =
-            _invalidStartNameCharacters.SelectAsTestParameters(x => x + "Element");
+            TestData.InvalidStartNameCharacters.SelectAsTestParameters(x => x + "Element");
 
         public static readonly IEnumerable<object[]> Invalid_Names =
-            TestHelper.CartesianProduct(_validStartNameCharacters, _invalidNameCharacters)
+            TestHelper.CartesianProduct(TestData.ValidStartNameCharacters, TestData.InvalidNameCharacters)
                 .SelectAsTestParameters(x => new[] { x.Item1 + $"Elem{x.Item2}ent", x.Item1 + "Elem" });
 
         public static readonly IEnumerable<object[]> Valid_Start_Name_Character_Names =
-            _validStartNameCharacters.SelectAsTestParameters(x => x + "Element");
+            TestData.ValidStartNameCharacters.SelectAsTestParameters(x => x + "Element");
 
         public static readonly IEnumerable<object[]> Valid_Names_Containing_Name_Only_Characters =
-            TestHelper.CartesianProduct(_validStartNameCharacters, _validNameInvalidStartNameIntersection)
+            TestHelper.CartesianProduct(TestData.ValidStartNameCharacters, _validNameInvalidStartNameIntersection)
                 .SelectAsTestParameters(x => x.Item1 + $"Elem{x.Item2}ent");
 
         public static readonly IEnumerable<object[]> Valid_Names_Containing_Start_Name_Characters =
-            TestHelper.CartesianProduct(_validStartNameCharacters, _validNameCharacters)
+            TestHelper.CartesianProduct(TestData.ValidStartNameCharacters, TestData.ValidNameCharacters)
                 .SelectAsTestParameters(x => x.Item1 + $"Elem{x.Item2}ent");
 
         [Theory]
