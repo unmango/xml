763716c8b32e03cab2324c3fe9c308e0533498e8
Erik Rasmussen <ecr.referee@hotmail.com>
2020-03-10 13:31:41 -0500

    Reorganize reader tests

---
+++ test/UnMango.Xml.Test/TestHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnMango.Xml.Test
+{
+    internal static class TestHelper
+    {
+        public static IEnumerable<(T, T)> CartesianProduct<T>(IEnumerable<T> first, IEnumerable<T> second)
+        {
+            foreach (var i in first)
+            {
+                foreach (var j in second)
+                {
+                    yield return (i, j);
+                }
+            }
+        }
+
+        public static IEnumerable<object[]> SelectAsTestParameters<T, V>(
+            this IEnumerable<T> source,
+            Func<T, IEnumerable<V>> selector)
+        {
+            return source.Select(selector).Select(x => x.Cast<object>().ToArray());
+        }
+
+        public static IEnumerable<object[]> SelectAsTestParameters<T>(
+            this IEnumerable<T> source,
+            Func<T, string> selector)
+        {
+            return source.Select(selector).Select(x => new object[] { x });
+        }
+
+        public static IEnumerable<object[]> SelectAsTestParameters<T>(
+            this IEnumerable<T> source,
+            Func<T, object> selector)
+        {
+            return source.Select(selector).Select(x => new object[] { x });
+        }
+    }
+}
