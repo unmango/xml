28c77dc1a9e5e294cc3d3c66916a4753f32548c6
UnstoppableMango <ecr.referee@hotmail.com>
2020-03-10 22:36:48 -0500

    ReadEntityValue Tests (#14)
    * Fix unnecessary inversion
    
    * Add some non-happy test cases
    
    * Remove ErrorProne.NET.Structs
    
    It don't seem to be workin

---
+++ test/UnMango.Xml.Test/Reader/ReadEntityValueTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnMango.Xml.Test.Reader
+{
+    [Trait("Category", "Unit")]
+    public class ReadEntityValueTests
+    {
+        [Theory]
+        [InlineData("\"Entity\"")]
+        [InlineData("'Entity'")]
+        public void Valid_Entity_Value(string value)
+        {
+            var bytes = Encoding.UTF8.GetBytes(value);
+            var reader = new XmlReader(bytes);
+
+            var resultSpan = reader.ReadEntityValue();
+
+            var result = Encoding.UTF8.GetString(resultSpan);
+            Assert.Equal("Entity", result);
+        }
+
+        [Theory]
+        [InlineData("!Entity")]
+        [InlineData("#Entity")]
+        [InlineData("&Entity")]
+        [InlineData("(Entity")]
+        public void Invalid_Start_Character(string value)
+        {
+            var bytes = Encoding.UTF8.GetBytes(value);
+
+            Assert.Throws<XmlParsingException>(() =>
+            {
+                var reader = new XmlReader(bytes);
+                reader.ReadEntityValue();
+            });
+        }
+
+        [Theory]
+        [InlineData("'&Entity")]
+        [InlineData("'%Entity")]
+        public void Invalid_Entity_Character(string value)
+        {
+            var bytes = Encoding.UTF8.GetBytes(value);
+
+            Assert.Throws<XmlParsingException>(() =>
+            {
+                var reader = new XmlReader(bytes);
+                reader.ReadEntityValue();
+            });
+        }
+
+        [Theory]
+        [InlineData("\"Entity")]
+        [InlineData("'Entity")]
+        [InlineData("\"Entity'")]
+        [InlineData("'Entity\"")]
+        public void Expect_Closing_Delimeter(string value)
+        {
+            var bytes = Encoding.UTF8.GetBytes(value);
+
+            Assert.Throws<XmlParsingException>(() =>
+            {
+                var reader = new XmlReader(bytes);
+                reader.ReadEntityValue();
+            });
+        }
+    }
+}
