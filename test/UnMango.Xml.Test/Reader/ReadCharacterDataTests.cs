4f0cbbb7f7d6124da0607e400e2d6034318ef027
Erik Rasmussen <ecr.referee@hotmail.com>
2020-03-11 15:52:05 -0500

    Add some character data tests

---
+++ test/UnMango.Xml.Test/Reader/ReadCharacterDataTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using Xunit;
+
+namespace UnMango.Xml.Test.Reader
+{
+    [Trait("Category", "Unit")]
+    public class ReadCharacterDataTests
+    {
+        [Theory]
+        [InlineData("Character Data")]
+        public void Read(string data)
+        {
+            var bytes = Encoding.UTF8.GetBytes(data);
+            var reader = new XmlReader(bytes);
+
+            var resultSpan = reader.ReadCharacterData();
+
+            var result = Encoding.UTF8.GetString(resultSpan);
+            Assert.Equal(data, result);
+        }
+
+        [Theory]
+        [InlineData("<Character Data")]
+        [InlineData("&Character Data")]
+        public void Invalid_Start_Character(string data)
+        {
+            var bytes = Encoding.UTF8.GetBytes(data);
+            var reader = new XmlReader(bytes);
+
+            var result = reader.ReadCharacterData();
+
+            Assert.Equal(0, result.Length);
+        }
+
+        [Theory]
+        [InlineData("Character]]> Data")]
+        public void Invalid_Sequence(string data)
+        {
+            var bytes = Encoding.UTF8.GetBytes(data);
+            var reader = new XmlReader(bytes);
+
+            var resultSpan = reader.ReadCharacterData();
+
+            var result = Encoding.UTF8.GetString(resultSpan);
+            Assert.Equal("Character", result);
+        }
+    }
+}
