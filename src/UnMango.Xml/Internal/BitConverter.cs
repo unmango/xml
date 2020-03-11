d56d3f04f31718acc9bbe4b022f70933fd82cce8
UnstoppableMango <ecr.referee@hotmail.com>
2020-03-11 14:34:10 -0500

    Add internal BitConverter for netstandard2.0 (#15)
    * Swap literal with bitops!
    
    * Fix exception message
    
    * Use internal BitConverter for netstandard2.0

---
+++ src/UnMango.Xml/Internal/BitConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace UnMango.Xml.Internal
+{
+    /// <summary>
+    /// https://github.com/dotnet/runtime/blob/5ea5b57374363b1cd8bdecc04d6cd6ba0290db50/src/libraries/System.Private.CoreLib/src/System/BitConverter.cs
+    /// </summary>
+    internal static class BitConverter
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static char ToChar(ReadOnlySpan<byte> value)
+        {
+            if (value.Length < sizeof(char))
+            {
+                throw new ArgumentOutOfRangeException(nameof(value));
+            }
+
+            return Unsafe.ReadUnaligned<char>(ref MemoryMarshal.GetReference(value));
+        }
+    }
+}
