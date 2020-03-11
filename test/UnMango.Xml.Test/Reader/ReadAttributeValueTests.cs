71968e6cb214268c1e39a9f4b700a66a4e8db485
Erik Rasmussen <ecr.referee@hotmail.com>
2020-03-11 14:43:36 -0500

    Copy entity tests and rename to attribute

---
+++ test/UnMango.Xml.Test/Reader/ReadAttributeValueTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using Xunit;
+
+namespace UnMango.Xml.Test.Reader
+{
+    [Trait("Category", "Unit")]
+    public class ReadAttributeValueTests
+    {
+        [Theory]
+        [InlineData("\"Attribute\"")]
+        [InlineData("'Attribute'")]
+        public void Valid_Attribute_Value(string value)
+        {
+            var bytes = Encoding.UTF8.GetBytes(value);
+            var reader = new XmlReader(bytes);
+
+            var resultSpan = reader.ReadAttributeValue();
+
+            var result = Encoding.UTF8.GetString(resultSpan);
+            Assert.Equal("Attribute", result);
+        }
+
+        [Theory]
+        [InlineData("!Attribute")]
+        [InlineData("#Attribute")]
+        [InlineData("&Attribute")]
+        [InlineData("(Attribute")]
+        public void Invalid_Start_Character(string value)
+        {
+            var bytes = Encoding.UTF8.GetBytes(value);
+
+            Assert.Throws<XmlParsingException>(() =>
+            {
+                var reader = new XmlReader(bytes);
+                reader.ReadAttributeValue();
+            });
+        }
+
+        [Theory]
+        [InlineData("'&Attribute")]
+        [InlineData("'%Attribute")]
+        public void Invalid_Attribute_Character(string value)
+        {
+            var bytes = Encoding.UTF8.GetBytes(value);
+
+            Assert.Throws<XmlParsingException>(() =>
+            {
+                var reader = new XmlReader(bytes);
+                reader.ReadAttributeValue();
+            });
+        }
+
+        [Theory]
+        [InlineData("\"Attribute")]
+        [InlineData("'Attribute")]
+        [InlineData("\"Attribute'")]
+        [InlineData("'Attribute\"")]
+        public void Expect_Closing_Delimiter(string value)
+        {
+            var bytes = Encoding.UTF8.GetBytes(value);
+
+            Assert.Throws<XmlParsingException>(() =>
+            {
+                var reader = new XmlReader(bytes);
+                reader.ReadAttributeValue();
+            });
+        }
+    }
+}
