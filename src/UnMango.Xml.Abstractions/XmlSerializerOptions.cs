3638db75db15d32771dc0cce29539e05fefb0ea9
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-24 22:14:37 -0500

    WIP setting up new proj

--- src/UnMango.Xml.Abstractions/XmlSerializerOptions.cs
+++ src/UnMango.Xml.Abstractions/XmlSerializerOptions.cs
@@ -1,11 +1,14 @@
-﻿namespace UnMango.Xml
+﻿using JetBrains.Annotations;
+
+namespace UnMango.Xml
 {
     /// <summary>
     /// Provides options to be used with <see cref="XmlSerializer"/>.
     /// </summary>
+    [PublicAPI]
     public sealed class XmlSerializerOptions
     {
-        internal static readonly XmlSerializerOptions DefaultOptions = new XmlSerializerOptions();
+        public static readonly XmlSerializerOptions DefaultOptions = new();
 
         /// <summary>
         /// Initializes a new instance of a <see cref="XmlSerializerOptions"/>.
--- src/UnMango.Xml/XmlSerializer.cs
+++ src/UnMango.Xml/XmlSerializer.cs
@@ -1,9 +1,12 @@
-﻿namespace UnMango.Xml
+﻿using JetBrains.Annotations;
+
+namespace UnMango.Xml
 {
     /// <summary>
     /// Provides functionality to serialize objects or value types to XML
     /// and deserialize XML into objects or value types.
     /// </summary>
+    [PublicAPI]
     public static partial class XmlSerializer
     {
         // Implementations of [De]Serialize[Async] are located in /Serializer
