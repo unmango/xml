e7cc56a7f3d8c44198e78cec6b9ecbbe3f644dab
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-25 21:18:56 -0500

    Fix benchmark

--- test/UnMango.Xml.Benchmarks/XmlWriterBenchmark.cs
+++ test/UnMango.Xml.Benchmarks/XmlWriterBenchmark.cs
@@ -1,27 +1,34 @@
 ﻿using System.Buffers;
 using System.Text;
+using System.Xml;
 using BenchmarkDotNet.Attributes;
 
 namespace UnMango.Xml.Benchmarks
 {
     public class XmlWriterBenchmark
     {
+        private const int Iterations = 10_000;
+        
         [Benchmark(Baseline = true)]
+        public void System_WriteTrue()
+        {
+            var stringBuilder = new StringBuilder(4);
+            var writer = System.Xml.XmlWriter.Create(stringBuilder, new() {
+                ConformanceLevel = ConformanceLevel.Fragment
+            });
+
+            for (var i = 0; i < Iterations; i++)
+                writer.WriteValue(true);
+        }
+        
+        [Benchmark]
         public void UnMango_WriteTrue()
         {
             var bufferWriter = new ArrayBufferWriter<byte>(4);
             var writer = new XmlWriter(bufferWriter);
 
-            writer.WriteTrue();
-        }
-
-        [Benchmark]
-        public void System_WriteTrue()
-        {
-            var stringBuilder = new StringBuilder(4);
-            var writer = System.Xml.XmlWriter.Create(stringBuilder);
-
-            writer.WriteValue(true);
+            for (var i = 0; i < Iterations; i++)
+                writer.WriteTrue();
         }
     }
 }
