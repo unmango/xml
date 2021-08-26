584a74df5a1447f02bdee9931b0dfc996d4b1027
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-25 20:39:26 -0500

    Mor stuff

--- src/UnMango.Xml/Internal/BitConverter.cs
+++ src/UnMango.Xml/Internal/BitConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if NETSTANDARD2_0
+using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -21,3 +22,4 @@ namespace UnMango.Xml.Internal
         }
     }
 }
+#endif
