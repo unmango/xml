2335684154880a08c42f717a9b0e03ed3ff94a4c
Erik Rasmussen <ecr.referee@hotmail.com>
2021-08-26 22:35:49 -0500

    Adding tokens I guess

--- src/UnMango.Xml/XmlToken.cs
+++ src/UnMango.Xml/XmlToken.cs
@@ -1,9 +1,19 @@
-﻿namespace UnMango.Xml
+﻿using JetBrains.Annotations;
+
+namespace UnMango.Xml
 {
     /// <summary>
     /// An XML token.
     /// </summary>
+    [PublicAPI]
     public enum XmlToken : byte
     {
+        None = 0,
+        
+        /// <summary> < </summary>
+        BeginElement = 1,
+        
+        /// <summary> > </summary>
+        EndElement = 2,
     }
 }
